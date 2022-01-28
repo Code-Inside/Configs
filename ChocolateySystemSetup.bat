c8b7c6b8dc922ea9220ee066a4e5e70b013a7349
removedporn <86824510+removedporn@users.noreply.github.com>
2022-01-28 22:34:43 +0800

    Delete ChocolateySystemSetup.bat

--- ChocolateySystemSetup.bat
+++
@@ -1,9 +0,0 @@
-::Install chocolatey from https://chocolatey.org/ first.
-::browsers
-choco install googlechrome firefox 
-::casual
-choco install skype
-::misc
-choco install 7zip paint.net filezilla gifcam 
-::code
-choco install notepadplusplus.install git.install github atom mssqlservermanagementstudio2014express
